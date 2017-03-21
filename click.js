52229831eb5214ea0148a42c26be1a60781aa1bc
Carlos Torres <8ctorres@stpatrick.org>
2017-03-21 10:54:17 -0500

    Add files via upload

---
+++ click.js
@@ -0,0 +1,15 @@
+//declare samecolor() here
+function samecolor(){
+
+const doc = document
+let divs = doc.querySelectorAll(#samecolor > div)
+
+for(let i=0;i<divs.length;i++){
+divs[i].style.color = #990000;
+}
+}
+//declare diffcolor() here
+
+function diffcolor(){
+
+}

057257093800d4ab8325b05e80626427fbfcc509
Carlos Torres <8ctorres@stpatrick.org>
2017-03-22 11:27:36 -0500

    Add files via upload

--- click.js
+++ click.js
@@ -1,11 +1,12 @@
 //declare samecolor() here
 function samecolor(){
 
-const doc = document
-let divs = doc.querySelectorAll(#samecolor > div)
+const doc = document;
+let divs = doc.querySelectorAll("#samecolor div");
 
 for(let i=0;i<divs.length;i++){
-divs[i].style.color = #990000;
+divs[i].style.backgroundColor = "#990011";
+console.log(i)
 }
 }
 //declare diffcolor() here

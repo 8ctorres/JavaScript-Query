3081ac52d8a59d1d12523b4b2a1b9183ef660e22
Carlos Torres <8ctorres@stpatrick.org>
2017-03-24 10:55:37 -0500

    Add files via upload

--- click.js
+++ click.js
@@ -5,8 +5,8 @@ const doc = document;
 let divs = doc.querySelectorAll("#samecolor > div");
 
 for(let i=0;i<divs.length;i++){
-divs[i].style.backgroundColor = "#ff0000";
-
+divs[i].style.backgroundColor = "#14e120";
+divs[i].style.opacity = (i/5+.25)
 }
 }
 
@@ -16,8 +16,29 @@ function diffcolor(){
 const doc = document;
 let divs = doc.querySelectorAll("#diffcolor div");
 
+if (divs[0].style.backgroundColor=="ffa500") {
+
 for(let i=0;i<divs.length;i=i+2){
-divs[i].style.backgroundColor = "#ffffff";
-divs[i+1].style.backgroundColor = "#0000ff";
+
+divs[i].style.backgroundColor = "#005aff";
+divs[i+1].style.backgroundColor = "#ffa500";
 }
+}
+
+else if(divs[0].style.backgroundColor=="005aff") {
+
+for(let i=0;i<divs.length;i=i+2){
+divs[i].style.backgroundColor = "#ffa500";
+divs[i+1].style.backgroundColor = "#005aff";
+}
+}
+
+else {
+    for(let i=0;i<divs.length;i=i+2){
+
+    divs[i].style.backgroundColor = "#005aff";
+    divs[i+1].style.backgroundColor = "#ffa500";
+    }
+}
+
 }
--- resize.js
+++ resize.js
@@ -4,10 +4,20 @@ tabletSize(){
     let divs = doc.querySelectorAll("div")
 
     for(let i=0;i<divs.length;i++){
-    divs[i].height = ;
-    divs[i].width = ;
+    divs[i].style.height = "75%";
+    divs[i].style.width = "75%" ;
     }
     }
 }
 
 //Declare phoneSize() here
+phoneSize(){
+    const doc = document;
+    let divs = doc.querySelectorAll("div")
+
+    for(let i=0;i<divs.length;i++){
+    divs[i].style.height = "50%";
+    divs[i].style.width = "50%" ;
+    }
+    }
+}
